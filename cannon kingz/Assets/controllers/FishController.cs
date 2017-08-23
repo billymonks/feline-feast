82eea9eee2b8d9d0e98ad0552c2cdce444ee7a79
Billy Monks <williamtmonks@gmail.com>
2017-08-23 17:43:34 -0400

    Added floppy fish

---
+++ cannon kingz/Assets/controllers/FishController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+
+	public float durability;
+	public int score = 10;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -15)
+			Break ();
+	}
+	public void ChildCollision(GameObject o, Collision c)
+	{
+		if(c.relativeVelocity.magnitude > 2)
+			durability -= c.relativeVelocity.magnitude;
+
+		if (durability <= 0)
+			Break ();
+	}
+
+	void Break()
+	{
+
+		BallisitaController.score += (int)(score * (1.0 + (BallisitaController.combo / 10.0)));
+		BallisitaController.combo += 1;
+		Destroy (gameObject);
+	}
+}
---
+++ cannon kingz/Assets/controllers/FishPieceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishPieceController : MonoBehaviour {
+	FishController fish;
+	// Use this for initialization
+	void Start () {
+		fish = GetComponentInParent<FishController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		fish.ChildCollision (this.gameObject, c);
+	}
+}
